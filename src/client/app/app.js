43e6981bf8a57fb82d4209a4f8a55919bc888642
Garan Guillory <garanguillory@gmail.com>
2016-04-29 22:39:16 -0600

    removing old comments.

--- src/client/app/app.js
+++ src/client/app/app.js
@@ -1,20 +1,3 @@
 var app = angular.module('MeanRedditClone',['ngRoute']);
 
 
-
-
-// app.config(function(authProvider){
-
-// 	authProvider.init({
-// 		domain: '',
-// 		clientID: '',
-// 		callbackURL: ''
-//		loginUrl: '/login'
-// 	});
-
-// })
-
-// .run(function(auth){
-//		auth.hookEvents();
-// })
-
